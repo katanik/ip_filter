cb1be7f3489f87d6cc60394c9555864dac12237a
katanik <katia.rulit@yandex.ru>
2021-08-31 18:12:52 +0700

    Fix #3

--- ip_filter.cpp
+++ ip_filter.cpp
@@ -5,13 +5,7 @@
 #include <vector>
 #include <algorithm>
 #include <sstream>
-
-// ("",  '.') -> [""]
-// ("11", '.') -> ["11"]
-// ("..", '.') -> ["", "", ""]
-// ("11.", '.') -> ["11", ""]
-// (".11", '.') -> ["", "11"]
-// ("11.22", '.') -> ["11", "22"]
+#include <fstream>
 
 using StringVector = std::vector<std::string>;
 
@@ -21,7 +15,7 @@ StringVector split(const std::string& str, char delim)
     std::stringstream ss;
     ss<<str;
 
-    for(std::string ipPart; std::getline(ss, ipPart, '.');)
+    for(std::string ipPart; std::getline(ss, ipPart, delim);)
     {
         result.push_back(ipPart);
     }
@@ -29,7 +23,39 @@ StringVector split(const std::string& str, char delim)
     return result;
 }
 
-void printOut(StringVector ip, char delim)
+void sortIpPool(std::vector<StringVector>& ipPool)
+{
+    auto filter = [](const auto& ip1, const auto& ip2)
+    {
+        auto it1 = ip1.begin(), it2 = ip2.begin();
+        for (; it1 != ip1.end() && it2 != ip2.end(); ++it1, ++it2)
+        {
+            auto ipPart1 = stoi(*it1);
+            auto ipPart2 = stoi(*it2);
+            if (ipPart1 == ipPart2)
+                continue;
+
+            return ipPart1 > ipPart2;    
+        }
+
+        return ip1.size() < ip2.size(); 
+    };
+
+    std::sort(ipPool.begin(), ipPool.end(), filter);
+}
+
+std::vector<StringVector> inputData(std::istream& in)
+{
+    std::vector<StringVector> ipPool;
+    for(std::string line; std::getline(in, line);)
+    {
+        std::vector<std::string> pureIp = split(line, '\t');
+        ipPool.push_back(split(pureIp.at(0), '.'));
+    }
+    return ipPool;
+}
+
+void printOut(StringVector ip, char delim, std::ostream& out)
 {
     if (ip.empty())
         return;
@@ -39,109 +65,43 @@ void printOut(StringVector ip, char delim)
 
     for(auto ipPart : ip)
     {
-        std::cout << ipPart << delim;
+        out << ipPart << delim;
     }
 
-    std::cout << lastIpPart;
+    out << lastIpPart;
+}
+
+void printResult(const std::vector<StringVector>& ipPool, std::ostream& out)
+{
+    for(const auto& ip : ipPool)
+    {
+        printOut(std::move(ip), '.', out);
+        out << std::endl;
+    }
 }
 
 int main(int argc, char const *argv[])
 {
     try
     {
-        std::vector<StringVector> ipPool;
-
-        for(std::string line; std::getline(std::cin, line);)
+        std::fstream inputFile("ip_filter.tsv");
+        if (!inputFile.is_open())
         {
-            ipPool.push_back(split(line, '.'));
+            std::cout << "failed to open " << '\n';
+            return 1;
         }
 
-        auto filter = [](const auto& ip1, const auto& ip2)
+        std::ofstream outputFile;
+        outputFile.open("result.txt", std::fstream::out);
+        if (!outputFile.is_open())
         {
-            auto it1 = ip1.begin(), it2 = ip2.begin();
-            for (; it1 != ip1.end() && it2 != ip2.end(); ++it1, ++it2)
-            {
-                auto ipPart1 = stoi(*it1);
-                auto ipPart2 = stoi(*it2);
-                if (ipPart1 == ipPart2)
-                    continue;
-
-                return ipPart1 > ipPart2;    
-            }
-
-            return ip1.size() > ip2.size(); 
-        };
-
-        std::sort(ipPool.begin(), ipPool.end(), filter);
-
-        for(const auto& ip : ipPool)
-        {
-            printOut(ip, '.');
-            std::cout << std::endl;
+            std::cout << "failed to create file " << '\n';
+            return 1;
         }
 
-        // 222.173.235.246
-        // 222.130.177.64
-        // 222.82.198.61
-        // ...
-        // 1.70.44.170
-        // 1.29.168.152
-        // 1.1.234.8
-
-        // TODO filter by first byte and output
-        // ip = filter(1)
-
-        // 1.231.69.33
-        // 1.87.203.225
-        // 1.70.44.170
-        // 1.29.168.152
-        // 1.1.234.8
-
-        // TODO filter by first and second bytes and output
-        // ip = filter(46, 70)
-
-        // 46.70.225.39
-        // 46.70.147.26
-        // 46.70.113.73
-        // 46.70.29.76
-
-        // TODO filter by any byte and output
-        // ip = filter_any(46)
-
-        // 186.204.34.46
-        // 186.46.222.194
-        // 185.46.87.231
-        // 185.46.86.132
-        // 185.46.86.131
-        // 185.46.86.131
-        // 185.46.86.22
-        // 185.46.85.204
-        // 185.46.85.78
-        // 68.46.218.208
-        // 46.251.197.23
-        // 46.223.254.56
-        // 46.223.254.56
-        // 46.182.19.219
-        // 46.161.63.66
-        // 46.161.61.51
-        // 46.161.60.92
-        // 46.161.60.35
-        // 46.161.58.202
-        // 46.161.56.241
-        // 46.161.56.203
-        // 46.161.56.174
-        // 46.161.56.106
-        // 46.161.56.106
-        // 46.101.163.119
-        // 46.101.127.145
-        // 46.70.225.39
-        // 46.70.147.26
-        // 46.70.113.73
-        // 46.70.29.76
-        // 46.55.46.98
-        // 46.49.43.85
-        // 39.46.86.85
-        // 5.189.203.46
+        auto ipPool = inputData(inputFile);
+        sortIpPool(ipPool);
+        printResult(ipPool, outputFile);
     }
     catch(const std::exception &e)
     {
