6ec2f014576d8577bff40929c6ee215884a061cf
katanik <katia.rulit@yandex.ru>
2021-09-01 13:09:23 +0700

    Added standard input / output #7

--- main.cpp
+++ main.cpp
@@ -4,17 +4,49 @@
 
 #include "lib/ip_list.h"
 
+void runFilters(std::ostream& out, IpList& ipList)
+{
+    // Print sorted ip list
+    ipList.sort();
+    ipList.print(out, IpFilter{});
+
+    // Print if the first bite is 1
+    IpFilter ipFilter1{ [](const IpData& ip) { return stoi(ip.at(0)) == 1; }};
+    ipList.print(out, ipFilter1);
+
+    // Print if the first bite is 46, the second is 70
+    IpFilter ipFilter2{ 
+        [](const IpData& ip)
+        {
+            return stoi(ip.at(0)) == 46 && stoi(ip.at(1)) == 70;
+        }};
+    ipList.print(out, ipFilter2);
+
+    // Print if any bite is 46
+    IpFilter ipFilter3{ 
+        [](const IpData& ip)
+        {
+            for (int i = 0; i < 4; ++i)
+            {
+                if (stoi(ip.at(i)) == 46)
+                    return true;
+            }
+            return false;
+        }};
+    ipList.print(out, ipFilter3);
+}
+
 int main(int argc, char const *argv[])
 {
     try
     {
+        /*
         std::fstream inputFile("ip_filter.tsv");
         if (!inputFile.is_open())
         {
             std::cout << "failed to open " << '\n';
             return 1;
         }
-
         std::ofstream outputFile;
         outputFile.open("result.txt", std::fstream::out);
         if (!outputFile.is_open())
@@ -22,39 +54,13 @@ int main(int argc, char const *argv[])
             std::cout << "failed to create file " << '\n';
             return 1;
         }
+        */
 
         // Read ip list
         IpList ipList{};
-        ipList.inputData(inputFile);
+        ipList.inputData(std::cin);
 
-        // Print sorted ip list
-        ipList.sort();
-        ipList.print(outputFile, IpFilter{});
-
-        // Print if the first bite is 1
-        IpFilter ipFilter1{ [](const IpData& ip) { return stoi(ip.at(0)) == 1; }};
-        ipList.print(outputFile, ipFilter1);
-
-        // Print if the first bite is 46, the second is 70
-        IpFilter ipFilter2{ 
-            [](const IpData& ip)
-            {
-                return stoi(ip.at(0)) == 46 && stoi(ip.at(1)) == 70;
-            }};
-        ipList.print(outputFile, ipFilter2);
-
-        // Print if any bite is 46
-        IpFilter ipFilter3{ 
-            [](const IpData& ip)
-            {
-                for (int i = 0; i < 4; ++i)
-                {
-                    if (stoi(ip.at(i)) == 46)
-                        return true;
-                }
-                return false;
-            }};
-        ipList.print(outputFile, ipFilter3);
+        runFilters(std::cout, ipList);
     }
     catch(const std::exception &e)
     {
